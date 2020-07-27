d4be73bb3ef2f9d1b3e8eeeced61fc09abd12616
Miguel Segura <mianse@hotmail.com>
2020-07-27 17:25:18 -0500

    Pokedex

---
+++ src/Components/PokedexScreen.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import Stat from './Stat'
+import LoadingPokemon from '../img/loading.gif'
+import ErrorPokemon from '../img/error.gif'
+import '../styles/PokedexScreen.css'
+
+// En este componente iteraremos a travez de la lista de actividades o similares
+function PokedexScreen({ pokemon, loading, error }){
+
+  if(error){
+    return (
+      <div className="pokedex-screen">
+        <img
+          src={ErrorPokemon}
+          alt="Hubo un error buscando tu pokemon"
+          className="pokedex-no-screen"
+        />
+      </div>
+    )
+  }
+
+  return (
+    <div className="pokedex-screen">
+      { !pokemon || loading ?
+        <img
+          src={LoadingPokemon}
+          alt="Aun no hay ningun pokeom"
+          className="pokedex-no-screen"
+        /> :
+        <div className="pokemon-info">
+          <h2 className="pokemon-name">{pokemon.name}</h2>
+          <img className="pokemon-img" src={pokemon.sprites.front_default} alt={pokemon.name} />
+          <ul className="pokemon-stats">
+            {pokemon.stats.map(item => <Stat key={item.stat.name} item={item}/>)}
+          </ul>
+        </div>
+      }
+    </div>
+  )
+}
+
+export default PokedexScreen
---
+++ src/Components/PokemonForm.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import '../styles/PokemonForm.css'
+
+function PokemonForm({ setPokemonId, setLoading, setError }){
+  const [ pokemon, setPokemon ] = useState('')
+
+  const handleChange = e => {
+    setPokemon(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if(pokemon !== ''){
+      setError(false)
+      setLoading(true)
+      setPokemonId(pokemon)
+      setPokemon('')
+      return
+    }
+    setError(true)
+  }
+
+  return (
+    <form className="pokemon-form" onSubmit={handleSubmit}>
+      <input
+        className="pokemon-input"
+        type="text"
+        name="pokemon"
+        value={pokemon}
+        placeholder="Busca tu pokemon"
+        onChange={handleChange}
+        autoComplete="off"/>
+      <input type="submit" className="pokemon-btn" value=""/>
+    </form>
+  )
+}
+
+export default PokemonForm
